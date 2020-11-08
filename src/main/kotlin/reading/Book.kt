42f83d7cdcf3782fd23941ac368062d04de01c1b
wraithras <logan.razclan1@gmail.com>
2020-11-08 12:21:24 -0700

    Lesson 5: part 5

--- src/main/kotlin/reading/Book.kt
+++ src/main/kotlin/reading/Book.kt
@@ -10,6 +10,10 @@ class Book (val title: String, val author: String, val year: String) {
     }
 }
 
+fun createBookSet(): Set<String> {
+    return setOf("Tristram Adventures", "Tristram Adventures 2")
+}
+
 fun main() {
     val book = Book("Tristram Adventures",
     "Deckard Cain",
@@ -18,4 +22,14 @@ fun main() {
     val (title, author, year) = book.getTriple()
 
     println("Here is your book $title written by $author in $year")
+
+    // Get a set of books
+    val allBooks = createBookSet()
+    val library: Map<String, Set<String>> = mutableMapOf("Deckard Cain" to allBooks)
+    println(library.any { it.value.contains("Tristram Adventures") })
+
+    val moreBooks = mutableMapOf<String, String>("Wilhelm Tell" to "Schiller")
+    moreBooks.getOrPut("Jungle Book") { "Kipling"}
+    moreBooks.getOrPut("Hamlet") { "Shakespeare" }
+    println(moreBooks)
 }
